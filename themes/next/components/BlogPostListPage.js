15ceabe6ab5f6dbf3db112526d0497ff735d7ea7
繁花漫画 <110377680+fanhua-manhua@users.noreply.github.com>
2022-09-05 15:56:45 +1000

    Update BlogPostListPage.js

--- themes/next/components/BlogPostListPage.js
+++ themes/next/components/BlogPostListPage.js
@@ -19,7 +19,7 @@ const BlogPostListPage = ({ page = 1, posts = [], postCount }) => {
     return <BlogPostListEmpty />
   } else {
     return (
-      <div id="container" className='w-full'>
+      <div id="container" className='w-full pt-10'>
         {/* 文章列表 */}
         <div className="space-y-6 px-2 pt-10">
           {posts.map(post => (

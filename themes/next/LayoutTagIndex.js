1d0dc83d24482f538c7368d01701bfd646a3413d
繁花漫画 <110377680+fanhua-manhua@users.noreply.github.com>
2022-09-05 17:46:39 +1000

    Update LayoutTagIndex.js

--- themes/next/LayoutTagIndex.js
+++ themes/next/LayoutTagIndex.js
@@ -6,7 +6,7 @@ export const LayoutTagIndex = (props) => {
   const { tags } = props
   const { locale } = useGlobal()
   return <LayoutBase {...props}>
-    <div className='bg-white dark:bg-zinc-700 px-10 py-10 shadow h-full'>
+    <div className='hadow hover:shadow dark:text-gray-300 border dark:border-black rounded-xl px-2 py-2 bg-white dark:bg-hexo-black-gray lg:duration-100 px-10 py-10 shadow h-full'>
       <div className='dark:text-gray-200 mb-5'><i className='fas fa-tags mr-4'/>{locale.COMMON.TAGS}:</div>
       <div id='tags-list' className='duration-200 flex flex-wrap'>
         { tags.map(tag => {

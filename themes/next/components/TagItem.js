5ed94d1369ef05a190f41392f7ff1072f84c4a89
繁花漫画 <110377680+fanhua-manhua@users.noreply.github.com>
2022-09-05 20:16:39 +1000

    Update TagItem.js

--- themes/next/components/TagItem.js
+++ themes/next/components/TagItem.js
@@ -10,7 +10,7 @@ const TagItem = ({ tag, selected }) => {
   return (
   <Link href={selected ? '/' : `/tag/${encodeURIComponent(tag.name)}`} passHref>
     <li
-      className={`notion-${tag.color}_background dark:bg-gray-700 list-none cursor-pointer rounded-md  
+      className={`notion-${tag.color}_background border dark:border-black dark:bg-hexo-black-gray list-none cursor-pointer rounded-md  
       duration-200 mr-1 my-1 px-2 py-1 text-sm whitespace-nowrap 
        hover:bg-gray-200 dark:hover:bg-gray-800 `}>
       <div className='text-gray-600 dark:text-gray-300 dark:hover:text-white'>
